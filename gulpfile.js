d84bbf8ae0badc12d8a85ea903ed1feb31e1f7da
warlyware <danward@gmail.com>
2016-05-19 22:30:14 -0400

    Add Sass to gulp, add gulp-connect, add live reload, add angular, clean up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,40 +4,47 @@ var gulp = require('gulp');
 var config = require('./gulp/config.json');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
-var reload = browserSync.reload;
+var connect = require('gulp-connect');
+
 
 // watch files for changes and reload
 
 gulp.task('styles', function() {
-    gulp.task('sass', function () {
-      return gulp.src(config.paths.src.styles)
+    return gulp.src(config.paths.src.styles)
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('build'));
-    });
+        .pipe(gulp.dest('build'))
+        .pipe(connect.reload());
 });
 
 gulp.task('copy:html', function() {
   return gulp.src(config.paths.src.html)
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest('build'))
+    .pipe(connect.reload());
 });
 
 gulp.task('copy:js', function() {
   return gulp.src(config.paths.src.js)
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest('build'))
+    .pipe(connect.reload());
+});
+
+gulp.task('copy:node_modules', function() {
+  return gulp.src(config.paths.node_modules)
+    .pipe(gulp.dest('build/node_modules'))
+    .pipe(connect.reload());
 });
 
 gulp.task('serve', function() {
-    browserSync({
-        server: {
-            baseDir: 'build'
-        }
+    connect.server({
+        root: 'build',
+        livereload: true
     });
 });
 
 gulp.task('watch', function() {
-    gulp.watch(config.paths.src.html, {cwd: 'src/app'}, reload);
-    gulp.watch(config.paths.src.js, {cwd: 'src/app'}, reload);
-    gulp.watch(config.paths.src.styles, ['sass'], reload);
+    gulp.watch(config.paths.src.html, ['copy:html']);
+    gulp.watch(config.paths.src.js, ['copy:js']);
+    gulp.watch(config.paths.src.styles, ['styles']);
 });
 
-gulp.task('default', ['styles', 'copy:html', 'copy:js', 'serve', 'watch']);
+gulp.task('default', ['styles', 'copy:html', 'copy:js', 'copy:node_modules', 'serve', 'watch']);
---
+++ src/app/application.module.js
@@ -0,0 +1,6 @@
+angular.module('profile-jonward', [
+    'components'
+])
+    .run(function() {
+        console.log('app online');
+    });
