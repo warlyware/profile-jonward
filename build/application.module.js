96a543b756f37beaca947d9a275e25b39dc8d915
warlyware <danward@gmail.com>
2016-05-23 22:42:40 -0400

    Add built for pages

--- build/application.module.js
+++ build/application.module.js
@@ -1,6 +1,30 @@
 angular.module('profile-jonward', [
-    'components'
+    'ui.router',
+    'profile-jonward.components'
 ])
-    .run(function() {
-        console.log('app online');
-    });
+
+.config(['$stateProvider', function($stateProvider) {
+
+    $stateProvider
+        .state('application', {
+            abstract: 'true',
+            controller: 'ApplicationController',
+            controllerAs: 'appCtrl'
+        })
+        .state('landing', {
+            url: '/',
+            templateUrl: 'components/landing/landing.tpl.html',
+            controller: 'LandingController',
+            controllerAs: 'LandingCtrl'
+        })
+        .state('home', {
+            url: '/home',
+            templateUrl: 'components/home/home.tpl.html',
+            controller: 'HomeController',
+            controllerAs: 'HomeCtrl'
+        });
+}])
+
+.run([function() {
+    console.log('app online');
+}]);
