fa13b6c5edee6b30592a93df64a1cb8daa779db7
warlyware <danward@gmail.com>
2016-08-22 19:56:43 -0400

    Add garbage landing page to be replaced with good landing page from flex-animation repo

--- src/app/components/landing/landing.controller.js
+++ src/app/components/landing/landing.controller.js
@@ -1,4 +1,35 @@
 angular.module('profile-jonward.home')
     .controller('LandingController', [function() {
         console.log('Landing Ctrl online');
+
+        function animateMidsection() {
+            TweenLite.to($('.landing-split'), 0.5, {
+                borderTopWidth: '400',
+                borderBottomWidth: '0',
+                borderLeftWidth: '100',
+                borderRightWidth: '0',
+                borderStyle: 'solid',
+                x: 0
+            });
+            TweenLite.to($('.landing-left'), 0.5, {
+                paddingRight: '0'
+            });
+        }
+
+        function consoleLog() {
+            console.log('i am console log');
+        }
+
+        TweenLite.from($('#main'), 1, {
+            height: '5',
+            ease: Power3.easeOut,
+            onComplete: animateMidsection
+        });
+
+        TweenLite.from($('.landing-split'), 1   , {
+            height: '5',
+            ease: Power3.easeOut,
+            x: '100'
+        });
+
     }]);
